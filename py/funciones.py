c3e441cf8f4349a850d5b88e7d127f7b70e90bfd
Mateo Gallo <mateo.mgallo@gmail.com>
2025-05-09 01:31:04 -0500

    creacion de archivo para funciones python

---
+++ py/funciones.py
@@ -0,0 +1,8 @@
+import os
+
+def mover_carpeta(dir1:str):
+    carpeta=os.getcwd()
+    if not carpeta.endswith(dir1):
+        nueva_carpeta=carpeta.split(f"Proyecto_talento_tech")[0]
+        ruta = os.path.join(nueva_carpeta, 'Proyecto_talento_tech',dir1)                
+        os.chdir(ruta)
